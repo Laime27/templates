8e653edb14c3351d4277fef8b3b6935c5bfa9202
Laime27 <ce27.laime@gmail.com>
2025-07-20 11:53:41 -0500

    se instalo axios y sonner

--- template-react/src/App.tsx
+++ template-react/src/App.tsx
@@ -1,10 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "@/routes/route"; 
+import { Toaster } from "@/components/ui/sonner"
 
 const router = createBrowserRouter(routes);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster  />
+    </>
+  );
 }
 
 export default App;
